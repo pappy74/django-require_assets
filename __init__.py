f60a8aeb2594565ffaee8a6313b44619de98e7c1
Jason Persampieri <jason@persampieri.net>
2010-10-20 20:35:45 -0700

    'named' requires; 'requires_script' template tag

--- __init__.py
+++ __init__.py
@@ -45,11 +45,55 @@ def set_request_key(request):
 def get_request_key(request):
     #return request.session['REQUEST_ID']
     return request
-    
+
 # require these file(s)
-def requireFile(request, files, group="default"):
+def requireFile(request, files, group='default'):
+    # update prefixes
+    tag = ""
+    for filename in files:
+
+        # distinguish between css and js files
+        extension = os.path.splitext(filename)[1][1:].lower()
+
+        # add prefixes as specified
+        prefixes = REQUIRES['prefixes'].get(extension, {})
+        for prefix, substitution in prefixes.items():
+            if ( filename.startswith(prefix) ):
+                filename = substitution + filename
+                break
+
+        tag += _add_req(request, group, extension, filename, filename=filename)
+    return tag
+
+def requireBlock(request, blocktype, content, name, group='default'):
+    reqtype = {
+        'script': 'js',
+        'style': 'css',
+    }[blocktype]
+
+    tag = _add_req(request, group, reqtype, name, block=content )
+    return tag
+
+def _add_req(request, group, reqtype, unique_id, filename=None, block=None):
+    """
+    add a requirement (file or block)
+    """
     key = get_request_key(request)
-    #print "%s" % files
+
+    # prep the dicts for this key/group combination
+    requirements = requires.setdefault(key,{
+        'unique': {},
+        'groups': {},
+    })['groups'].setdefault(group,{
+        'css': [],
+        'js': [],
+    })
+    unique_reqs = requires[key]['unique']
+
+    # only include each requirement once
+    if unique_id in unique_reqs:
+        return ""
+    unique_reqs[unique_id] = True
 
     # make sure we have regular expressions for each file token
     if not group in token_regexes:
@@ -59,67 +103,67 @@ def requireFile(request, files, group="default"):
             token = token.replace("<INDEX>", '(\d+)')
             token_regexes[group][filetype] = re.compile(token)
 
-    # set up a shortcut var to handle each file we need to add
-    required_files = requires.setdefault(key,{
-        'unique': {},
-        'groups': {},
-    })['groups'].setdefault(group,{
-        'css': [],
-        'js': [],
-    })
-    unique_files = requires[key]['unique']
+    # add the requirement to the appropriate list
+    index = len(requirements[reqtype])
+    if reqtype == "js":
+        if filename:
+            req = JSFile(filename)
+        else:
+            req = JSBlock(block)
 
-    replace_with = ""
-    for filename in files:
-        
-        # distinguish between css and js files
-        extension = os.path.splitext(filename)[1][1:].lower()
-        
-        # add prefixes as specified
-        prefixes = REQUIRES['prefixes'].get(extension, {})
-        for prefix, substitution in prefixes.items():
-            if ( filename.startswith(prefix) ):
-                filename = substitution + filename
-                break
+    elif reqtype == "css":
+        if filename:
+            req = CSSFile(filename)
+        else:
+            req = CSSBlock(block)
 
-        # only include each file once
-        if filename in unique_files:
-            continue
-        unique_files[filename] = True
+    requirements[reqtype].append( req )
 
-        # add the file to the appropriate list
-        index = len(required_files[extension])
-        if extension == "js":
-            required_files[extension].append(JSFile(filename))
-        elif extension == "css":
-            required_files[extension].append(CSSFile(filename))
+    # build the token that gets embedded in the raw html
+    token = REQUIRES['file_tokens'][reqtype]
+    token = token.replace("<GROUP>", group)
+    token = token.replace("<INDEX>", str(index))
+    return token
 
-        # generate a 'token' that will be rendered in the HTML
-        # until the middleware does its pass
-        file_token = REQUIRES['file_tokens'][extension]
-        file_token = file_token.replace("<GROUP>", group)
-        file_token = file_token.replace("<INDEX>", str(index))
-        replace_with += file_token
-    
-    return replace_with
-
-class CSSFile:
+class RequiresFileObj:
     def __init__(self, filename):
         self.filename = filename
-    
+
+    def absoluteURL(self):
+        return re.match('^https?://', self.filename)
+
+    def compressible(self):
+        return not self.absoluteURL()
+
+class RequiresBlockObj:
+    def __init__(self, block):
+        self.block = block
+
+    def compressible(self):
+        return True
+
+class CSSFile(RequiresFileObj):
     def render(self):
         return REQUIRES['css_template'] % self.filename
-        
-class JSFile:
-    def __init__(self, filename):
-        self.filename = filename
 
+class CSSBlock(RequiresBlockObj):
     def render(self):
-        return REQUIRES['js_template'] % self.filename
-                
-# Replace all of the embedded placeholders within an html string with 
+        return "\t<style>%s</style>\n" % self.block
+
+class JSFile(RequiresFileObj):
+    def render(self):
+        if self.absoluteURL():
+            return "\t<script src='%s'></script>\n" % self.filename
+        else:
+            return REQUIRES['js_template'] % self.filename
+
+class JSBlock(RequiresBlockObj):
+    def render(self):
+        return "\t<script>%s</script>\n" % self.block
+
+# Replace all of the embedded placeholders within an html string with
 # link and script tags.
-# By default, it grabs the individual file placeholders, keeping track 
+# By default, it grabs the individual file placeholders, keeping track
 # of the order.  Then, it inserts a list of CSS link tags at the end of the
 # "head" and a list of JS script tags at the end of the "body"
 def process_html(request, html):
@@ -142,12 +186,16 @@ def _fix_html_type(request_key, html, filetype):
         regex = token_regexes[group][filetype]
         html = regex.sub(sub_func, html)
 
-        # replace the 'replace me' tag with actual list of 
+        # replace the 'replace me' tag with actual list of
         # 'tags' (ie <link href="foo.css">)
         file_html = u""
+        uncompressible_html = u""
         for index in indices:
             fileObj = files[index]
-            file_html += fileObj.render()
+            if fileObj.compressible():
+                file_html += fileObj.render()
+            else:
+                uncompressible_html += fileObj.render()
 
         # try to use the provided 'compress' app to compress the output
         if hasattr(settings, 'COMPRESS') and settings.COMPRESS:
@@ -158,15 +206,16 @@ def _fix_html_type(request_key, html, filetype):
             lexer = Lexer(file_html, None)
             from compressor.templatetags.compress import compress
             file_html = compress(
-                Parser(lexer.tokenize()), 
+                Parser(lexer.tokenize()),
                 Token(TOKEN_TEXT, "compress " + filetype)
             ).render({})
 
+        file_html = uncompressible_html + file_html
         tag = REQUIRES['placeholder_tags'][group].get(filetype, None)
         if tag:
             html = smart_unicode(html)
             html = html.replace(tag, file_html + tag)
-    
+
     return html
 
-              
+
--- templatetags/requires.py
+++ templatetags/requires.py
@@ -3,7 +3,7 @@ from django.conf import settings
 import os.path
 import logging
 
-from requires_js_css import requireFile
+from requires_js_css import requireFile, requireBlock
 
 register = template.Library()
 
@@ -23,6 +23,30 @@ def requires(parser, token):
         raise template.TemplateSyntaxError, "%r tag requires at least one argument" % token.contents.split()[0]
 
     return RequiresNode( group, files )
+
+# use this in include some javascript declared inline.  If an optional 'name'
+# parameter is provided, the script will only get included once
+@register.tag
+def requires_script(parser, token):
+    name = None
+    try:
+        args = token.split_contents()[1:]
+    except IndexError:
+        pass
+        
+    group = "'default'"
+    name = None
+    for arg in args:
+        if "=" in arg:
+            key,value = arg.split("=")
+            if key == 'group':
+                group = value
+        else:
+            name = arg
+            
+    nodelist = parser.parse(('endrequires_script',))
+    parser.delete_first_token()
+    return RequiresBlockNode( "script", group, name, nodelist )
     
 class RequiresNode(template.Node):
     def __init__(self, group, files):
@@ -36,4 +60,23 @@ class RequiresNode(template.Node):
             logging.error("No request object found: %s" % str(self.files))
             return ""
         tag = requireFile(request, self.files, self.group)
+        return tag
+
+class RequiresBlockNode(template.Node):
+    child_nodelists = ('nodelist',)
+    def __init__(self, blocktype, group, name, nodelist):
+        self.type = blocktype
+        self.group = template.Variable(group)
+        self.name = name
+        self.nodelist = nodelist
+    def render(self, context):
+        try:
+            request = context["request"]
+        except:
+            logging.error("No request object found: %s" % str(self.files))
+            return ""
+
+        content = self.nodelist.render(context)
+        group = self.group.resolve(context)
+        tag = requireBlock(request, self.type, content, self.name, group)
         return tag
